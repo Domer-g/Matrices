ed6529d5348c65aa45b2baab61094a742f895cfa
Domer <domeryt@gmail.com>
2025-05-30 13:54:05 +0200

    Added __getitem__
    get item fix

--- main.py
+++ main.py
@@ -29,5 +29,39 @@ class Matrix:
             return
         
         raise ValueError("None arguments given match __init__; Matrix not created")
+
+    def __getitem__(self, index: tuple[int|slice, int|slice] | int | slice) -> 'Matrix':
+
+        # return while only 'int' given
+        if(isinstance(index, int)): return Matrix([[self.get_item(index)]])
         
+        # return while only 'slice' given
+        if(isinstance(index, slice)):
+            start = index.start
+            stop = index.stop
+            step = index.step if (index.step != None) else 1
+            return Matrix([[ self.get_item(i) for i in range(start, stop, step) ]])
+
+        if(not isinstance(index, tuple)): raise ValueError("None arguments given match __getitem__; Nothing returned")
+
+        # return if 'int, slice' or 'int,  int'
+        if(isinstance(index[0], int)):
+            if(index[0] >= self.rows): raise ValueError("Index out of range")
+            if(isinstance(index[1], int)):
+                if(index[1] >= self.columns): raise ValueError("Index out of range")
+                return self[index[0]*self.columns + index[1]]
+            if(isinstance(index[1], slice)):
+                return self[ (index[0]*self.columns):((index[0]+1)*self.columns)]
+        
+        # return if 'slice, int'
+        if(isinstance(index[0], slice) and isinstance(index[1], int)):
+            if(index[1] >= self.columns): raise ValueError("Index out of range")
+            return self[ index[1]:(self.columns*self.rows):self.columns]
+
+        raise ValueError("None arguments given match __getitem__; Nothing returned")
     
+
+    def get_item(self, index: int) -> Any:
+        return self.__data[index // self.columns][index % self.columns]
+
+

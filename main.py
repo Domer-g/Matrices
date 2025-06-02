eb86944a03a8f601cd4af70a61545a23fef4b8ad
Domer <domeryt@gmail.com>
2025-06-02 21:38:31 +0200

    get/set item fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Any, Iterator
 # https://www.pythonmorsels.com/every-dunder-method/#context-managers
 
 class Matrix:
-    def __Matrix_size_check(A: 'Matrix', B: 'Matrix') -> bool: # type: ignore
+    def __Matrix_same_size_check(A: 'Matrix', B: 'Matrix') -> bool: # type: ignore
         if(A.rows != B.rows): return False
         if(A.columns != B.columns): return False
         return True
@@ -37,12 +37,18 @@ class Matrix:
         raise ValueError("None arguments given match __init__; Matrix not created")
 
     def __getitem__(self, index: tuple[int|slice, slice|int] | int | slice) -> 'Matrix | Any':
-        """Gets an entry from the matrix, return a Matrix"""
+        """Gets an entry from the matrix.        
+        - If (int), returns the stored value at that position. (look iterator for indexing)
+        - If (slice), returns a Matrix containing elements of coresponding indecies.
+        - If (int, int), returns the stored value at that position.
+        - If (int, slice), returns a row as a Matrix.
+        - If (slice, int), returns a column as a Matrix.
+        """
 
-        # return while only 'int' given
+        # return while only 'int' given; returns any
         if(isinstance(index, int)): return self.get_item(index)
         
-        # return while only 'slice' given
+        # return while only 'slice' given; returns matrix
         if(isinstance(index, slice)):
             start = index.start
             stop = index.stop
@@ -53,16 +59,11 @@ class Matrix:
 
         # return if 'int, slice' or 'int,  int'
         if(isinstance(index[0], int)):
-            if(index[0] >= self.rows): raise ValueError("Index out of range")
-            if(isinstance(index[1], int)):
-                if(index[1] >= self.columns): raise ValueError("Index out of range")
-                return self[index[0]*self.columns + index[1]]
-            if(isinstance(index[1], slice)):
-                return self[ (index[0]*self.columns):((index[0]+1)*self.columns) ]
+            if(isinstance(index[1], int)): return self[ index[0]*self.columns + index[1] ]
+            if(isinstance(index[1], slice)): return self[ (index[0]*self.columns):((index[0]+1)*self.columns) ]
         
         # return if 'slice, int'
-        if(isinstance(index[0], slice) and isinstance(index[1], int)):
-            if(index[1] >= self.columns): raise ValueError("Index out of range")
+        if(isinstance(index[0], slice) and isinstance(index[1], int)): 
             return self[ index[1]:(self.columns*self.rows):self.columns ]
 
         raise ValueError("None arguments given match __getitem__; Nothing returned")
@@ -70,13 +71,11 @@ class Matrix:
     def __setitem__(self, index: tuple[int|slice, int|slice] | int | slice, other: 'list[Any] | Any | Matrix' ) -> None:
         """
         Allows for setting values
-        [int] - set from index of entry (look iterator)
-        [slice] - sets all tenries to other, or only the slice when given a list
-        [int,int] - sets the given entry to other
-        [int, slice] - sets the row to other
-        [slice, int] - sets the column to other
-
-        other can be a list or a Matrix when using slices 
+        - [int] - set from index of entry (look iterator for indexing)
+        - [slice] - sets all entries to other, or only the slice when given a list
+        - [int, int] - sets the given entry to other
+        - [int, slice] - sets the row to other. List/matrix or any
+        - [slice, int] - sets the column to other. List/matrix or any
         """
         # handle list assignments
         if(isinstance(other, (list, Matrix))):
@@ -86,18 +85,16 @@ class Matrix:
                 start = index.start
                 stop = index.stop
                 step = index.step if (index.step != None) else 1
-                # [ self.get_item(i) for i in range(start, stop, step) ]
 
                 if(abs(stop-start) != len(other)-1): raise ValueError("List length missmatch")
 
-                for i in range(start, stop, step):
-                    self.__data[i // self.columns][i % self.columns] = other[i]
+                for i in range(start, stop, step): self.__setitem(i, other[i])
                 return
             
             # set if 'int, slice'
             if(not isinstance(index, tuple)): raise ValueError("None arguments given match __setitem__; Nothing set")
+            
             if(isinstance(index[0], int) and isinstance(index[1], slice)):
-                if(index[0] >= self.rows): raise ValueError("Index out of range")
                 if(len(other) != self.columns): raise ValueError("List length missmatch")
                 for i in range(self.columns):
                     self.__data[index[0]][i] = other[i]
@@ -105,7 +102,6 @@ class Matrix:
             
             # set if 'slice, int'
             if(isinstance(index[0], slice) and isinstance(index[1], int)):
-                if(index[1] >= self.columns): raise ValueError("Index out of range")
                 if(len(other) != self.rows): raise ValueError("List length missmatch")
                 for i in range(self.rows):
                     self.__data[i][index[1]] = other[i]
@@ -116,29 +112,27 @@ class Matrix:
 
         # set while only 'int' given
         if(isinstance(index, int)): 
-            self.__data[index // self.columns][index % self.columns] = other
+            self.__setitem(index, other)
             return 
         
         # set while only 'slice' given
         if(isinstance(index, slice)):
             # stes all item to other
             if((index.start == None) and (index.stop == None) and (index.step == None)):
-                self.__data = [ [other for j in range(self.columns)]  for i in range(self.rows) ]
+                self.__data = [ [other for j in range(self.columns)] for i in range(self.rows) ]
                 return
-            start = index.start
-            stop = index.stop
+            start = index.start if (index.step != None) else 0
+            stop = index.stop if (index.step != None) else len(self)
             step = index.step if (index.step != None) else 1
             for i in range(start, stop, step):
-                self.__data[i // self.columns][i % self.columns] = other
+                self.__setitem(i, other)
             return
 
         if(not isinstance(index, tuple)): raise ValueError("None arguments given match __setitem__; Nothing set")
 
         # set if 'int, slice' or 'int,  int'
         if(isinstance(index[0], int)):
-            if(index[0] >= self.rows): raise ValueError("Index out of range")
             if(isinstance(index[1], int)):
-                if(index[1] >= self.columns): raise ValueError("Index out of range")
                 self.__data[index[0]][index[1]] = other
                 return
             if(isinstance(index[1], slice)):
@@ -148,7 +142,6 @@ class Matrix:
         
         # set if 'slice, int'
         if(isinstance(index[0], slice) and isinstance(index[1], int)):
-            if(index[1] >= self.columns): raise ValueError("Index out of range")
             for i in range(self.rows):
                 self.__data[i][index[1]] = other
             return
@@ -198,10 +191,13 @@ class Matrix:
     def __setitem(self, index: int, other: Any) -> None:
         self.__data[index // self.columns][index % self.columns] = other
 
+    def __setitem2(self, index0: int, index1: int, other: Any) -> None:
+        self.__data[index0][index1] = other
+
     # Arthmetic operations
     def __add__(self, other: 'Matrix | Any') -> 'Matrix':
         if(isinstance(other, Matrix)):
-            if(not Matrix.__Matrix_size_check(self, other)): raise ValueError("Cannot add matrices that are not of the same dimention")
+            if(not Matrix.__Matrix_same_size_check(self, other)): raise ValueError("Cannot add matrices that are not of the same dimention")
             outcome: Matrix = Matrix(self)
             for i, elem in enumerate(other): outcome.__setitem(i, outcome.get_item(i)+elem)
             return outcome
@@ -212,7 +208,7 @@ class Matrix:
     
     def __sub__(self, other: 'Matrix | Any') -> 'Matrix':
         if(isinstance(other, Matrix)):
-            if(not Matrix.__Matrix_size_check(self, other)): raise ValueError("Cannot add matrices that are not of the same dimention")
+            if(not Matrix.__Matrix_same_size_check(self, other)): raise ValueError("Cannot add matrices that are not of the same dimention")
             outcome: Matrix = Matrix(self)
             for i, elem in enumerate(other): outcome.__setitem(i, outcome.get_item(i)-elem)
             return outcome
